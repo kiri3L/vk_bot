db7afdfff3840faf835aadd06fc32e35d94f9208
k3l <klubimoff@gmail.com>
2019-03-27 11:47:30 +0300

    add method wrappers

--- src/Main.py
+++ src/Main.py
@@ -1,51 +1,21 @@
-import requests
 import time
-from tokens import token_group
+import vk_methods
 
-r2 = requests.get('https://api.vk.com/method/groups.getLongPollServer?',
-                  params={'group_id': '179378864',
-                          'access_token': token_group,
-                          'v': '5.92'})
+connect = vk_methods.connect_to_server()
+r3 = vk_methods.get_updates(connect[0], connect[1], connect[2])
 
-print(r2)
-print(r2.text)
-print(r2.json())
+while 1:
+    sender = vk_methods.get_sender_id(r3.text)
+    if sender:
+        vk_methods.get_attachments(r3.text)
+        name = vk_methods.get_sender_info(sender)['first_name']
+        if name == 'Андрей':
+            vk_methods.send_messages(sender, 'Привет, Андрей. Ну где ты был, ну обними меня скорей')
+        else:
+            vk_methods.send_messages(sender, "Привет, " + name, image='photo-179378864_456239017')
 
-serv = r2.json()['response']['server']
-key = r2.json()['response']['key']
-ts = r2.json()['response']['ts']
-
-serv += '?'
-
-print(serv, ts)
-
-while 5 < 10:
-    r3 = requests.get(serv, params={'act': 'a_check', 'key': key, 'ts': ts})
-    print(r3.json())
-    if 'updates' in r3.json():
-        l = list(r3.json()['updates'])
-        for d in l:
-            if 'object' in d:
-                print('Победа')
-                if 'from_id' in d['object']:
-                    r = requests.get('https://api.vk.com/method/users.get?',
-                                     params={'access_token': token_group,
-                                    'v': '5.92',
-                                    'user_ids': d['object']['from_id']})
-                    print(r.json())
-                    r = requests.get('https://api.vk.com/method/messages.send?',
-                                     params={'user_id': d['object']['from_id'],
-                                             'random_id': 0,
-                                             'message': 'Привет',
-                                             'group_id': '179378864',
-                                             'access_token': token_group,
-                                             'v': '5.92',
-                                             })
-                    print(r)
-                    print(r.text)
     ts = r3.json()['ts']
     time.sleep(10)
-
-
+    r3 = vk_methods.get_updates(connect[0], ts, connect[2])
 
 
---
+++ src/vk_methods.py
@@ -0,0 +1,93 @@
+import requests
+import re
+from tokens import token_group
+
+
+def get_sender_id(updates):
+    r = re.search(r'from_id\":\d+', updates)
+    if r:
+        return re.search(r'\d+', r[0])[0]
+    else:
+        return None
+
+
+def get_attachments(updates):
+    print("I work ")
+    r = re.search(r"attachments\":\[.+\]", updates)
+    if r:
+        r = re.search(r"\[.+\]", r[0])
+
+    else: print(" but i @#%^!")
+
+
+def get_sender_info(id):
+    r = requests.get('https://api.vk.com/method/users.get?',
+                     params={'access_token': token_group,
+                             'v': '5.92',
+                             'user_ids': id})
+    return r.json()['response'][0]
+
+
+def send_messages(id, text, image=None, music=None):
+    # if ( image != None ) & ( music != None ):
+    #     requests.get('https://api.vk.com/method/messages.send?',
+    #                  params={'user_id': id,
+    #                          'random_id': 0,
+    #                          'message': text,
+    #                          'attachment': image + ',' + music,
+    #                          'group_id': '179378864',
+    #                          'access_token': token_group,
+    #                          'v': '5.92'})
+    if image:
+        requests.get('https://api.vk.com/method/messages.send?',
+                     params={'user_id': id,
+                             'random_id': 0,
+                             'message': text,
+                             'attachment': image,
+                             'group_id': '179378864',
+                             'access_token': token_group,
+                             'v': '5.92'})
+    # elif music:
+    #     print(' Music ' + music)
+    #     r = requests.get('https://api.vk.com/method/messages.send?',
+    #                      params={'user_id': id,
+    #                              'random_id': 0,
+    #                              'message': text,
+    #                              'attachment': music,
+    #                              'group_id': '179378864',
+    #                              'access_token': token_group,
+    #                              'v': '5.92'})
+    #     print(r.text)
+    #     print(r.json())
+    else:
+        requests.get('https://api.vk.com/method/messages.send?',
+                     params={'user_id': id,
+                             'random_id': 0,
+                             'message': text,
+                             'group_id': '179378864',
+                             'access_token': token_group,
+                             'v': '5.92'})
+
+
+def connect_to_server():
+    r2 = requests.get('https://api.vk.com/method/groups.getLongPollServer?',
+                      params={'group_id': '179378864',
+                              'access_token': token_group,
+                              'v': '5.92'})
+
+    print(r2)
+    print(r2.text)
+    print(r2.json())
+
+    serv = r2.json()['response']['server']
+    key = r2.json()['response']['key']
+    ts = r2.json()['response']['ts']
+
+    serv += '?'
+    return serv, ts, key
+
+
+def get_updates(serv, ts, key):
+    r3 = requests.get(serv, params={'act': 'a_check', 'key': key, 'ts': ts})
+    return r3
+
